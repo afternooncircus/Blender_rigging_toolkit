ee8c0c2a70a98396463f51aa47396744ec8750d3
Frederick Solano <afternooncircus@gmail.com>
2024-03-06 02:09:38 -0300

    deleting double assignment

--- operators/op_import_armature_presets.py
+++ operators/op_import_armature_presets.py
@@ -16,17 +16,19 @@ class HumanArmaturePreset(Operator):
     def execute(self, context):
         current_dir = Path(__file__).parent.parent
         preset_folder = rf'{current_dir}\armature_presets\human_preset.blend'
-        blendpath = preset_folder
         armature_name = 'human_preset'
 
         #Code by Zender on Blenderartist.
-        with bpy.data.libraries.load(blendpath, link=False) as (data_from, data_to):
+        with bpy.data.libraries.load(preset_folder, link=False) as (data_from, data_to):
             if not armature_name in {name for name in data_from.objects}:
                 self.report({"ERROR"}, f"There is no armature named '{armature_name}'")
-                return {"CANCELLED"} 
+                return {"CANCELLED"}
+            
             data_to.objects = [name for name in data_from.objects if name in armature_name]
+
         for obj in data_to.objects:
             context.scene.collection.objects.link(obj)
+            
         self.report({"INFO"}, f"Succesfully imported armature '{armature_name}'")
         return {"FINISHED"} 
     
