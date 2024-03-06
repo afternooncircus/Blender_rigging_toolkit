2e34b98580c264c3148b001a8410cea6dd98014b
Frederick Solano <afternooncircus@gmail.com>
2024-03-06 02:05:03 -0300

    adding a complete armature preset and fixing directory path

--- operators/op_import_armature_presets.py
+++ operators/op_import_armature_presets.py
@@ -14,18 +14,20 @@ class HumanArmaturePreset(Operator):
         return context.mode == "OBJECT"
     
     def execute(self, context):
-        #Addon Folder
-        current_dir = Path.cwd()
-        #going to SubFolder
+        current_dir = Path(__file__).parent.parent
         preset_folder = rf'{current_dir}\armature_presets\human_preset.blend'
         blendpath = preset_folder
-        namelist = ['Armature']
+        armature_name = 'human_preset'
 
         #Code by Zender on Blenderartist.
         with bpy.data.libraries.load(blendpath, link=False) as (data_from, data_to):
-            data_to.objects = [name for name in data_from.objects if name in namelist] 
+            if not armature_name in {name for name in data_from.objects}:
+                self.report({"ERROR"}, f"There is no armature named '{armature_name}'")
+                return {"CANCELLED"} 
+            data_to.objects = [name for name in data_from.objects if name in armature_name]
         for obj in data_to.objects:
             context.scene.collection.objects.link(obj)
+        self.report({"INFO"}, f"Succesfully imported armature '{armature_name}'")
         return {"FINISHED"} 
     
 
